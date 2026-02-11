e2817ecf0b5a3765bae76f927e39331add09b3c1
Vinnakota Madhuri krishna priya <VinnakotaMkpriya@mictech.edu.in>
2026-02-11 20:13:08 +0530

    Initial backend setup with Django, DRF, JWT and roles

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Vessel, Port
+
+admin.site.register(Vessel)
+admin.site.register(Port)
